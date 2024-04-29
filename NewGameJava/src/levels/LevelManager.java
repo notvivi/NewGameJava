51ef1bc7dbec3097e632ee21844e556440855b81
notvivi <tomanova@spsejecna.cz>
2024-04-29 19:42:18 +0200

    added methods that draw whole level sprites

--- NewGameJava/src/levels/LevelManager.java
+++ NewGameJava/src/levels/LevelManager.java
@@ -1,15 +1,47 @@
 package levels;
 
 import main.Game;
+import utilz.LoadSave;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class LevelManager {
     private Game game;
-    private BufferedImage levelSprite;
+    private BufferedImage[] levelSprite;
+    private Level levelOne;
 
     public LevelManager(Game game) {
         this.game = game;
-       // levelSprite =
+        importOutsideSprites();
+        levelOne = new Level(LoadSave.getDataAboutLevel());
     }
+
+    private void importOutsideSprites() {
+        BufferedImage image = LoadSave.getSpriteAtlas(LoadSave.LEVEL_ATLAS);
+        levelSprite = new BufferedImage[48];
+        for(int i = 0; i < 4;i++){
+            for(int j = 0; j < 12;j++){
+                int index =  i*12 + j;
+                levelSprite[index] = image.getSubimage(j*32,i*32,32,32);
+            }
+        }
+    }
+
+    public void draw(Graphics g){
+        for(int i = 0; i < Game.TILES_IN_HEIGHT;i++){
+            for(int j = 0; j < Game.TILES_IN_WIDTH;j++){
+                int index = levelOne.getSpriteIndex(j,i);
+                g.drawImage(levelSprite[index],j*Game.TILES_SIZE,i*Game.TILES_SIZE,Game.TILES_SIZE,Game.TILES_SIZE,null);
+            }
+        }
+    }
+    public void update(){
+
+    }
+
+    public Level getCurrentLevel(){
+        return levelOne;
+    }
+
 }
