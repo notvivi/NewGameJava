2317f8667f4701ba8e10a6414e6263ac65a98d3d
notvivi <tomanova@spsejecna.cz>
2024-04-29 16:10:48 +0200

    adding hitBox methods

--- NewGameJava/src/entities/Entity.java
+++ NewGameJava/src/entities/Entity.java
@@ -1,40 +1,41 @@
 package entities;
 
 import java.awt.*;
+import java.awt.geom.Rectangle2D;
 
 public abstract class Entity {
 
     protected float x;
     protected float y;
     protected int width, height;
-    protected Rectangle hitBox;
+    protected Rectangle2D.Float hitBox;
     public Entity(float x,float y, int width, int height){
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
-        entityHitBox();
+        initHitBox(x, y, width, height);
     }
 
-    public void entityHitBox(){
-        hitBox = new Rectangle((int) x, (int) y,width,height);
+    public void initHitBox(float x, float y, float width, float height){
+        hitBox = new Rectangle2D.Float((int) x, (int) y,width,height);
     }
 
     public void drawHitBox(Graphics g){
         g.setColor(Color.PINK);
-        g.drawRect(hitBox.x,hitBox.y, hitBox.width, hitBox.height);
+        g.drawRect((int)hitBox.x,(int)hitBox.y, (int)hitBox.width, (int)hitBox.height);
     }
 
-    public void updateHitBox(){
-        hitBox.x = (int) x;
-        hitBox.y = (int) y;
-    }
+  //  public void updateHitBox(){
+   //     hitBox.x = (int) x;
+   //     hitBox.y = (int) y;
+  //  }
 
-    public Rectangle getHitBox() {
+    public Rectangle2D.Float getHitBox() {
         return hitBox;
     }
 
-    public void setHitBox(Rectangle hitBox) {
+    public void setHitBox(Rectangle2D.Float hitBox) {
         this.hitBox = hitBox;
     }
 }
