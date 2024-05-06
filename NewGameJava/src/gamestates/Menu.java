09b87038fc9105152512a84b2c9fb960ad31569a
notvivi <tomanova@spsejecna.cz>
2024-05-06 17:44:18 +0200

    moved here methods from class for moving player

--- NewGameJava/src/gamestates/Menu.java
+++ NewGameJava/src/gamestates/Menu.java
@@ -1,5 +1,57 @@
 package gamestates;
 
-public class Menu {
+import main.Game;
 
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+
+public class Menu extends State implements MethodsForStates {
+
+    public Menu(Game game) {
+        super(game);
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        g.setColor(Color.black);
+        g.drawString("MENU",Game.GAME_WIDTH / 2,200);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent keyEvent) {
+        if(keyEvent.getKeyCode() == KeyEvent.VK_ENTER){
+            GameState.state = GameState.PLAYING;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent keyEvent) {
+
+    }
 }
