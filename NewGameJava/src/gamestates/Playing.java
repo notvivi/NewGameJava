5f90cdd622abc5557e83347aba1db351f2377b4c
notvivi <tomanova@spsejecna.cz>
2024-05-06 17:45:33 +0200

    added methods

--- NewGameJava/src/gamestates/Playing.java
+++ NewGameJava/src/gamestates/Playing.java
@@ -1,4 +1,114 @@
 package gamestates;
 
-public class Playing {
+import entities.Player;
+import levels.LevelManager;
+import main.Game;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+
+public class Playing extends State implements MethodsForStates{
+
+    private LevelManager levelManager;
+    private Player player;
+
+    public Playing(Game game) {
+        super(game);
+        initClasses(game);
+    }
+
+    private void initClasses(Game game) {
+        levelManager = new LevelManager(game);
+        player = new Player(200,200,(int)(64 * Game.SCALE),(int)(40 * Game.SCALE));
+        player.loadLevelData(levelManager.getCurrentLevel().getLevelData());
+
+    }
+
+    public void windowFocusLost() {
+        player.resetDirectionBooleans();
+    }
+    public Player getPlayer(){
+        return player;
+    }
+
+
+    @Override
+    public void update() {
+        levelManager.update();
+        player.update();
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        levelManager.draw(g);
+        player.render(g);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent mouseEvent) {
+        if(mouseEvent.getButton() == MouseEvent.BUTTON1){
+            player.setAttack(true);
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent keyEvent) {
+        switch (keyEvent.getKeyCode()){
+            case KeyEvent.VK_A:
+                player.setLeft(true);
+                break;
+            case KeyEvent.VK_S:
+                player.setDown(true);
+                break;
+            case KeyEvent.VK_D:
+                player.setRight(true);
+                break;
+            case KeyEvent.VK_W:
+                player.setUp(true);
+                break;
+            case KeyEvent.VK_SPACE:
+                player.setJump(true);
+                break;
+            case KeyEvent.VK_BACK_SPACE:
+                GameState.state = GameState.MENU;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent keyEvent) {
+        switch (keyEvent.getKeyCode()){
+            case KeyEvent.VK_A:
+                player.setLeft(false);
+                break;
+            case KeyEvent.VK_S:
+                player.setDown(false);
+                break;
+            case KeyEvent.VK_D:
+                player.setRight(false);
+                break;
+            case KeyEvent.VK_W:
+                player.setUp(false);
+                break;
+            case KeyEvent.VK_SPACE:
+                player.setJump(false);
+                break;
+
+        }
+    }
 }
