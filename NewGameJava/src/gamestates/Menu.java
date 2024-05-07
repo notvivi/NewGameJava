c9d2d9bee2b35ebe99203e8cb657c691c73158bc
notvivi <tomanova@spsejecna.cz>
2024-05-08 00:23:08 +0200

    added whole menu with buttons

--- NewGameJava/src/gamestates/Menu.java
+++ NewGameJava/src/gamestates/Menu.java
@@ -1,26 +1,59 @@
 package gamestates;
 
 import main.Game;
+import ui.MenuButton;
+import utilz.LoadSave;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
 
 public class Menu extends State implements MethodsForStates {
 
+    private MenuButton[] menuButtons = new MenuButton[3];
+
+    private BufferedImage backgroundMenuImage;
+    private int menuX;
+    private int menuY;
+    private int menuWidth;
+    private int menuHeight;
+
+
     public Menu(Game game) {
         super(game);
+        loadButtons();
+        loadMenuBackground();
+    }
+
+    private void loadMenuBackground() {
+        backgroundMenuImage = LoadSave.getSpriteAtlas(LoadSave.MENU_BACKGROUND);
+        menuWidth = (int) (backgroundMenuImage.getWidth() * Game.SCALE);
+        menuHeight = (int) (backgroundMenuImage.getHeight() * Game.SCALE);
+        menuX = (int) ((int) (Game.GAME_WIDTH / 2) - menuWidth / 2);
+        menuY = (int) (45 * Game.SCALE);
+
+    }
+
+    private void loadButtons() {
+        menuButtons[0] = new MenuButton((int) (Game.GAME_WIDTH / 2.2),(int) (150 * Game.SCALE), 0, GameState.PLAYING);
+        menuButtons[1] = new MenuButton((int) (Game.GAME_WIDTH / 2.2),(int) (220 * Game.SCALE), 1, GameState.OPTIONS);
+        menuButtons[2] = new MenuButton((int) (Game.GAME_WIDTH / 2.2),(int) (290 * Game.SCALE), 2, GameState.QUIT);
     }
 
     @Override
     public void update() {
-
+        for(MenuButton mb: menuButtons){
+            mb.update();
+        }
     }
 
     @Override
     public void draw(Graphics g) {
-        g.setColor(Color.black);
-        g.drawString("MENU",Game.GAME_WIDTH / 2,200);
+        g.drawImage(backgroundMenuImage, menuX,menuY,menuWidth,menuHeight,null);
+        for(MenuButton mb: menuButtons){
+            mb.draw(g);
+        }
     }
 
     @Override
@@ -30,17 +63,44 @@ public class Menu extends State implements MethodsForStates {
 
     @Override
     public void mousePressed(MouseEvent mouseEvent) {
-
+        for(MenuButton menuButtons: menuButtons){
+            if(isInButton(mouseEvent,menuButtons)){
+                menuButtons.setMousePressed(true);
+                break;
+            }
+        }
     }
 
     @Override
     public void mouseReleased(MouseEvent mouseEvent) {
+        for(MenuButton menuBtn: menuButtons){
+            if(isInButton(mouseEvent,menuBtn)){
+                if(menuBtn.isMousePressed()){
+                    menuBtn.setGameState();
+                    break;
+                }
+            }
+        }
+        resetButtons();
+    }
 
+    private void resetButtons() {
+        for(MenuButton menuBtn: menuButtons){
+            menuBtn.resetBooleans();
+        }
     }
 
     @Override
     public void mouseMoved(MouseEvent mouseEvent) {
-
+        for(MenuButton menuBtn: menuButtons){
+            menuBtn.setMouseOver(false);
+        }
+        for(MenuButton menuBtn: menuButtons){
+            if(isInButton(mouseEvent,menuBtn)){
+                menuBtn.setMouseOver(true);
+                break;
+            }
+        }
     }
 
     @Override
--- NewGameJava/src/ui/MenuButton.java
+++ NewGameJava/src/ui/MenuButton.java
@@ -1,10 +1,82 @@
 package ui;
 
+import gamestates.GameState;
+import utilz.LoadSave;
+import utilz.Constants.Ui.Buttons;
+
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class MenuButton {
 
-    public MenuButton(int xPosition, int yPosition, int rowIndex){
+    private int xPosition;
+    private int yPosition;
+    private int rowIndex;
+    private int index = 0;
+    private int xOffSetCenter = Buttons.BUTTON_WIDTH_DEFAULT / 2;
+    private boolean mouseOver;
+    private boolean mousePressed;
+    public Rectangle buttonHitBox;
+    GameState gameState;
+    private BufferedImage images[];
 
+    public MenuButton(int xPosition, int yPosition, int rowIndex, GameState gameState){
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
+        this.rowIndex = rowIndex;
+        this.gameState = gameState;
+        loadImages();
+        isInHitBox();
+    }
+
+    private void isInHitBox() {
+        buttonHitBox = new Rectangle(xPosition - xOffSetCenter,yPosition,Buttons.BUTTON_WIDTH,Buttons.BUTTON_HEIGHT);
+    }
+
+
+    private void loadImages(){
+        images = new BufferedImage[3];
+        BufferedImage temp = LoadSave.getSpriteAtlas(LoadSave.MENU_BUTTON_ATLAS);
+        for(int i = 0; i < images.length;i++){
+            images[i] = temp.getSubimage(i * Buttons.BUTTON_WIDTH_DEFAULT,rowIndex * Buttons.BUTTON_HEIGHT_DEFAULT,Buttons.BUTTON_WIDTH_DEFAULT,Buttons.BUTTON_HEIGHT_DEFAULT);
+        }
+
+    }
+
+    public void draw(Graphics g){
+        update();
+        g.drawImage(images[index],xPosition - xOffSetCenter,yPosition,Buttons.BUTTON_WIDTH,Buttons.BUTTON_HEIGHT,null);
+    }
+    public void update(){
+        index = 0;
+        if(mouseOver){
+            index = 1;
+        } else if (mousePressed) {
+           index = 2;
+        }
+    }
+
+    public void setGameState(){
+        GameState.state = gameState;
+    }
+    public void resetBooleans(){
+        mouseOver = false;
+        mousePressed = false;
+    }
+
+    public boolean isMouseOver() {
+        return mouseOver;
+    }
+
+    public void setMouseOver(boolean mouseOver) {
+        this.mouseOver = mouseOver;
+    }
+
+    public boolean isMousePressed() {
+        return mousePressed;
+    }
+
+    public void setMousePressed(boolean mousePressed) {
+        this.mousePressed = mousePressed;
     }
 }
