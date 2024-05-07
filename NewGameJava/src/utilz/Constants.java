5cc13dbc935daab7aefcd3ae55e6edd275cf33a8
notvivi <tomanova@spsejecna.cz>
2024-05-08 00:21:37 +0200

    added more constants for buttons and menu

--- NewGameJava/src/utilz/Constants.java
+++ NewGameJava/src/utilz/Constants.java
@@ -1,8 +1,18 @@
 package utilz;
 
+import main.Game;
+
 public class Constants {
 
-
+    public static class Ui{
+        public static class Buttons{
+            public static final int BUTTON_WIDTH_DEFAULT = 140;
+            public static final int BUTTON_HEIGHT_DEFAULT = 56;
+            public static final int BUTTON_WIDTH = (int) (BUTTON_WIDTH_DEFAULT * Game.SCALE);
+            public static final int BUTTON_HEIGHT = (int) (BUTTON_HEIGHT_DEFAULT * Game.SCALE);
+            
+        }
+    }
     public static class Directions{
         public static final int LEFT = 0;
         public static final int UP = 1;
