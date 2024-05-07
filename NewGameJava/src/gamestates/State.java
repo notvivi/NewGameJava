8addb3007d05597aa043cc8194e3ae9c58034e95
notvivi <tomanova@spsejecna.cz>
2024-05-08 00:24:58 +0200

    added method that checks hitbox in button

--- NewGameJava/src/gamestates/State.java
+++ NewGameJava/src/gamestates/State.java
@@ -1,12 +1,18 @@
 package gamestates;
 
 import main.Game;
+import ui.MenuButton;
+
+import java.awt.event.MouseEvent;
 
 public class State {
     private Game game;
     public State(Game game){
         this.game = game;
+    }
 
+    public boolean isInButton(MouseEvent event, MenuButton menuButton){
+        return menuButton.buttonHitBox.contains(event.getX(),event.getY());
     }
 
     public Game getGame() {
