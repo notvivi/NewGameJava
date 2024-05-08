0dc2c9d4193f58db7bd067fedac44aa01da9ca10
notvivi <tomanova@spsejecna.cz>
2024-05-08 18:06:01 +0200

    made levels bigger

--- NewGameJava/src/levels/LevelManager.java
+++ NewGameJava/src/levels/LevelManager.java
@@ -28,11 +28,11 @@ public class LevelManager {
         }
     }
 
-    public void draw(Graphics g){
+    public void draw(Graphics g, int levelOffSet){
         for(int i = 0; i < Game.TILES_IN_HEIGHT;i++){
-            for(int j = 0; j < Game.TILES_IN_WIDTH;j++){
+            for(int j = 0; j < levelOne.getLevelData()[0].length;j++){
                 int index = levelOne.getSpriteIndex(j,i);
-                g.drawImage(levelSprite[index],j*Game.TILES_SIZE,i* Game.TILES_SIZE,Game.TILES_SIZE,Game.TILES_SIZE,null);
+                g.drawImage(levelSprite[index],j*Game.TILES_SIZE - levelOffSet,i* Game.TILES_SIZE,Game.TILES_SIZE,Game.TILES_SIZE,null);
             }
         }
     }
