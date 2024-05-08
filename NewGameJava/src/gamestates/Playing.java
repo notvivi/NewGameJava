157d68fd3a8cf58232edaae32dcbbbd977f85c4b
notvivi <tomanova@spsejecna.cz>
2024-05-08 19:05:52 +0200

    added big and small clouds

--- NewGameJava/src/gamestates/Playing.java
+++ NewGameJava/src/gamestates/Playing.java
@@ -4,27 +4,46 @@ import entities.Player;
 import levels.LevelManager;
 import main.Game;
 import utilz.LoadSave;
+import utilz.Constants.Environment;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.util.Random;
 
 public class Playing extends State implements MethodsForStates{
+
+    Random random = new Random();
 
     private LevelManager levelManager;
     private Player player;
 
     private int xLevelOffSet = 0;
-    private int leftBorder = (int) (0.2 * Game.GAME_WIDTH);
-    private int rightBorder = (int) (0.8 * Game.GAME_WIDTH);
-    private int levelTilesWide = LoadSave.getDataAboutLevel()[0].length;
-    private int maxTilesOffSet = levelTilesWide - Game.TILES_IN_WIDTH;
-    private int maxLevelOffSetX = maxTilesOffSet * Game.TILES_SIZE;
+    private final int leftBorder = (int) (0.2 * Game.GAME_WIDTH);
+    private final int rightBorder = (int) (0.8 * Game.GAME_WIDTH);
+    private final int levelTilesWide = LoadSave.getDataAboutLevel()[0].length;
+    private final int maxTilesOffSet = levelTilesWide - Game.TILES_IN_WIDTH;
+    private final int maxLevelOffSetX = maxTilesOffSet * Game.TILES_SIZE;
+
+    private final BufferedImage backgroundLevel;
+    private final BufferedImage bigClouds;
+    private final BufferedImage smallCloud;
+    private int[]smallCloudsPosition;
+
 
 
     public Playing(Game game) {
         super(game);
         initClasses(game);
+        backgroundLevel = LoadSave.getSpriteAtlas(LoadSave.LEVEL_BACKGROUND);
+        bigClouds = LoadSave.getSpriteAtlas(LoadSave.BIG_CLOUDS);
+        smallCloud = LoadSave.getSpriteAtlas(LoadSave.SMALL_CLOUD);
+        smallCloudsPosition = new int[8];
+        for(int i = 0; i < smallCloudsPosition.length; i++){
+            smallCloudsPosition[i] = (int) (90 * Game.SCALE) +random.nextInt((int) (100 * Game.SCALE));
+        }
+
     }
 
     private void initClasses(Game game) {
@@ -71,8 +90,19 @@ public class Playing extends State implements MethodsForStates{
 
     @Override
     public void draw(Graphics g) {
+        g.drawImage(backgroundLevel,0,0,Game.GAME_WIDTH,Game.GAME_HEIGHT,null);
+        drawClouds(g);
         levelManager.draw(g,xLevelOffSet);
         player.render(g,xLevelOffSet);
+    }
+
+    private void drawClouds(Graphics g) {
+        for(int i = 0; i < 3;i++){
+            g.drawImage(bigClouds, i * Environment.BIG_CLOUDS_WIDTH - (int) (xLevelOffSet * 0.3),(int) (204 * Game.SCALE), Environment.BIG_CLOUDS_WIDTH, Environment.BIG_CLOUDS_HEIGHT,null);
+        }
+        for(int i = 0; i < smallCloudsPosition.length; i++){
+            g.drawImage(smallCloud,Environment.SMALL_CLOUDS_WIDTH * 4 * i - (int) (xLevelOffSet * 0.7),smallCloudsPosition[i],Environment.SMALL_CLOUDS_WIDTH,Environment.SMALL_CLOUDS_HEIGHT,null);
+        }
     }
 
     @Override
