5490eed57f1164819bd88d259d64d7bf35a06570
notvivi <tomanova@spsejecna.cz>
2024-05-08 18:02:04 +0200

    added new input

--- NewGameJava/src/utilz/LoadSave.java
+++ NewGameJava/src/utilz/LoadSave.java
@@ -12,11 +12,14 @@ public class LoadSave {
 
     public static final String PLAYER_ATLAS = "res/player_sprites.png";
     public static final String LEVEL_ATLAS = "res/outside_sprites.png";
-    public static final String LEVEL_ONE_DATA = "res/level_one_data.png";
+   // public static final String LEVEL_ONE_DATA = "res/level_one_data.png";
+    public static final String LEVEL_ONE_DATA = "res/level_one_data_long.png";
     public static final String MENU_BUTTON_ATLAS = "res/button_atlas.png";
     public static final String MENU_BACKGROUND = "res/menu_background.png";
     public static final String MENU_POKEDEX = "res/menu_background_pokedex.png";
     public static final String START_BUTTON = "res/button_play.png";
+    public static final String PIKACHU_MENU_BACKGROUND = "res/pikachu_background.png";
+
 
     public static BufferedImage getSpriteAtlas(String fileName){
         BufferedImage image = null;
@@ -36,8 +39,8 @@ public class LoadSave {
     }
 
     public static int[][] getDataAboutLevel(){
-        int[][] levelData = new int[Game.TILES_IN_HEIGHT][Game.TILES_IN_WIDTH];
         BufferedImage image = getSpriteAtlas(LEVEL_ONE_DATA);
+        int[][] levelData = new int[image.getHeight()][image.getWidth()];
         for(int i = 0; i < image.getHeight();i++){
             for(int j = 0;j < image.getWidth();j++){
                 Color color = new Color(image.getRGB(j,i));
