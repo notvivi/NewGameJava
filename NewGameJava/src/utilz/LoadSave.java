b078374accbb8e9a5e79b2b5da3b0402468b3797
notvivi <tomanova@spsejecna.cz>
2024-05-08 15:44:48 +0200

    added new inputs

--- NewGameJava/src/utilz/LoadSave.java
+++ NewGameJava/src/utilz/LoadSave.java
@@ -15,6 +15,8 @@ public class LoadSave {
     public static final String LEVEL_ONE_DATA = "res/level_one_data.png";
     public static final String MENU_BUTTON_ATLAS = "res/button_atlas.png";
     public static final String MENU_BACKGROUND = "res/menu_background.png";
+    public static final String MENU_POKEDEX = "res/menu_background_pokedex.png";
+    public static final String START_BUTTON = "res/button_play.png";
 
     public static BufferedImage getSpriteAtlas(String fileName){
         BufferedImage image = null;
