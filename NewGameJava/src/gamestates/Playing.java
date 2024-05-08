e4769fda3aea42b2df628118508fdb9dd761da5b
notvivi <tomanova@spsejecna.cz>
2024-05-08 18:05:47 +0200

    added camera moving with player

--- NewGameJava/src/gamestates/Playing.java
+++ NewGameJava/src/gamestates/Playing.java
@@ -3,6 +3,7 @@ package gamestates;
 import entities.Player;
 import levels.LevelManager;
 import main.Game;
+import utilz.LoadSave;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
@@ -12,6 +13,14 @@ public class Playing extends State implements MethodsForStates{
 
     private LevelManager levelManager;
     private Player player;
+
+    private int xLevelOffSet = 0;
+    private int leftBorder = (int) (0.2 * Game.GAME_WIDTH);
+    private int rightBorder = (int) (0.8 * Game.GAME_WIDTH);
+    private int levelTilesWide = LoadSave.getDataAboutLevel()[0].length;
+    private int maxTilesOffSet = levelTilesWide - Game.TILES_IN_WIDTH;
+    private int maxLevelOffSetX = maxTilesOffSet * Game.TILES_SIZE;
+
 
     public Playing(Game game) {
         super(game);
@@ -37,12 +46,33 @@ public class Playing extends State implements MethodsForStates{
     public void update() {
         levelManager.update();
         player.update();
+        checkCloseToBorder();
+    }
+
+    private void checkCloseToBorder() {
+        int playerX = (int) player.getHitBox().x;
+        int difference = playerX - xLevelOffSet;
+
+        if(difference > rightBorder){
+            xLevelOffSet += difference - rightBorder;
+        } else if (difference < leftBorder) {
+            xLevelOffSet += difference - leftBorder;
+        }
+
+        if(xLevelOffSet > maxLevelOffSetX){
+            xLevelOffSet = maxLevelOffSetX;
+            
+        } else if (xLevelOffSet < 0) {
+            xLevelOffSet = 0;
+            
+        }
+
     }
 
     @Override
     public void draw(Graphics g) {
-        levelManager.draw(g);
-        player.render(g);
+        levelManager.draw(g,xLevelOffSet);
+        player.render(g,xLevelOffSet);
     }
 
     @Override
